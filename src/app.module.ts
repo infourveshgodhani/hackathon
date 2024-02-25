65b67069cbd12ff2ba39a52518c8a22d8297cd79
Urvesh G <urvesh.g@caratlane.com>
2024-02-25 19:17:23 +0530

    second

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ItemsModule } from './items/items.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     ItemsModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'viaduct.proxy.rlwy.net',
-      port: 29741,
-      username: 'postgres',
-      password: '223c4afFG4b5D43--54ccC-EAAAFabbe',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
       database: 'railway',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
